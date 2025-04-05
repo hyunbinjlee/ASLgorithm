fd58baef620f803506b1540742b210810f54fefa
hyunbinjlee <hyunbinlee@u.northwestern.edu>
2025-04-05 17:47:58 -0500

    Fix tailwind

--- src/utils/filter.js
+++ src/utils/filter.js
@@ -1,8 +1,6 @@
 import { GoogleGenerativeAI } from "@google/generative-ai";
 
-const GEMINI_API_KEY =
-  import.meta.env.VITE_GEMINI_API_KEY ||
-  "AIzaSyARoHt6dOcGF3EOFzckTcBewk1Kwp9xSUc";
+const GEMINI_API_KEY = import.meta.env.VITE_GEMINI_API_KEY;
 const genAI = new GoogleGenerativeAI(GEMINI_API_KEY);
 const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
 
